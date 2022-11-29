9102bd72d1a59d7a47015e9d542f2e045615e671
alexiscalivar <alexis999ok@gmail.com>
2022-11-29 00:52:11 -0300

    com

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,18 @@
+const Navbar = () => {
+    return (
+        <nav>
+            <h1>Ecommerce</h1>
+            <ul>
+                <li>
+                    Inicio
+                </li>
+                <li>
+                    Comprar
+                </li>
+                <li>
+                    Contacto
+                </li>
+            </ul>
+        </nav>
+    )
+}
